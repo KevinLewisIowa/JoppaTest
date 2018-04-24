1552de000cf6d1c078a8493f9bda123b172e13b0
KevinLewisIowa <kevinlewisiowa@gmail.com>
2018-04-23 21:30:58 -0500

    fixed bad url string

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -80,7 +80,7 @@ export class MainService {
 
   getRouteLocations(id): Observable<Location[]>{
     if(this.online){
-      return this.http.get(this.apiUrl + `getRouteLocations?routeId=${id}`)
+      return this.http.get(this.apiUrl + `locationsForRoute?routeId=${id}`)
       .map(res => {return res; })
       .catch(err => this.handleError(err));
     }
