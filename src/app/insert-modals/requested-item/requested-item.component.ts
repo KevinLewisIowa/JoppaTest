4eed57931f7f5a05fefb9e4eee61d79594c2829e
Tim Wilson <tim.wilson@joppa.org>
2018-11-11 20:12:07 -0600

    Add select list for commonly requested items with prompts for additional info if needed

--- src/app/insert-modals/requested-item/requested-item.component.ts
+++ src/app/insert-modals/requested-item/requested-item.component.ts
@@ -12,6 +12,9 @@ export class RequestedItemComponent implements OnInit {
   @ViewChild('requestedItemMdl') requestedItemMdl: ElementRef
   @Output() requestedItemAdded = new EventEmitter<RequestedItem>();
   description: string = '';
+  placeholderText: string = 'Test';
+  extraInfo: string = '';
+  extraInfoNeeded: boolean = false;
   constructor(private modalService: NgbModal, private service: ClientService) { }
 
   ngOnInit() {
@@ -21,19 +24,63 @@ export class RequestedItemComponent implements OnInit {
     this.modalService.open(this.requestedItemMdl,{ size: 'lg', backdrop: 'static'});
   }
 
+  onChange(selectedItem: string) {
+    switch (true) {
+      case selectedItem === 'Socks':
+        this.placeholderText = 'Size';
+        this.extraInfoNeeded = true;
+        break;
+      case selectedItem.includes('Batteries'):
+        this.placeholderText = 'Number Needed';
+        this.extraInfoNeeded = true;
+        break;
+      case selectedItem === 'Other':
+        this.placeholderText = 'Description'
+        this.extraInfoNeeded = true;
+        break;
+      default:
+        this.extraInfoNeeded = false;
+        break;
+    }
+  }
+
   submitItem() {
     const item = new RequestedItem();
     const clientId = sessionStorage.getItem('selectedClient');
-    if (this.description != null && !isNaN(Number(clientId))) {
-      item.item_description = this.description;
-      item.client_id = Number(clientId);
-      this.service.insertRequestedItem(item).subscribe((data: RequestedItem) => {
-        if (data != null && data.id != null) {
-          this.requestedItemAdded.emit(data);
+    if (this.description != null && this.description !== '' && !isNaN(Number(clientId))) {
+      if (this.extraInfoNeeded && (this.extraInfo === null || this.extraInfo === '')) {
+        alert('Need to enter additional info for this item');
+      }
+      else {
+        let itemDescription: string;
+        if (this.extraInfo != null && (this.description !== 'Other' && this.description !== 'Socks')) {
+          itemDescription = this.extraInfo + ' ' + this.description;
         }
-      });
+        else if (this.extraInfo != null && this.description === 'Other') {
+          itemDescription = this.extraInfo;
+        }
+        else if (this.extraInfo != null && this.description === 'Socks') {
+          itemDescription = 'Size ' + this.extraInfo + ' ' + this.description;
+        }
+        else {
+          itemDescription = this.description;
+        }
 
-      this.description = '';
+        item.item_description = itemDescription;
+        item.client_id = Number(clientId);
+        this.service.insertRequestedItem(item).subscribe((data: RequestedItem) => {
+          if (data != null && data.id != null) {
+            this.requestedItemAdded.emit(data);
+          }
+        });
+
+        this.description = '';
+        this.extraInfo = '';
+        this.extraInfoNeeded = false;
+      }
+    }
+    else {
+      alert('You need to enter an item');
     }
   }
 }
