22ab6f6a8f04a5d650bd942cdf3ec68b2de2ab75
Tim Wilson <wilsont3@central.edu>
2019-11-29 09:40:08 -0600

    Update api to use prod for prod

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -28,9 +28,7 @@ import { ClientPet } from 'app/models/client-pet';
 export class ClientService {
   theHeader = new HttpHeaders().set('Content-Type', 'application/json');
   online = true;
-  // private baseUrl = 'https://hidden-springs-63744.herokuapp.com/';  // URL to web api
-  // private apiUrl = 'https://hidden-springs-63744.herokuapp.com/';
-  private baseUrl = 'https://joppa-api-test.herokuapp.com/';
+  private baseUrl = 'https://joppa-api-prod.herokuapp.com/';
 
   constructor(private http: HttpClient, private store: Store<IMainStore>, private router: Router) { 
     console.log(this.baseUrl);
