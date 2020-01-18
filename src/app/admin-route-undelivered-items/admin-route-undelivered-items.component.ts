2fde16b7d41867780afd809e13e9179d8feeeb2c
Nic Addelia <addelia.nicolas@gmail.com>
2020-01-18 09:14:27 -0600

    Here's the work I've done so far on the undelivered items page. The mat-table format seems to be working between the pages themselves (the paginator is even picking up the number from the back end) but the website isn't playing nice with the format itself.

--- src/app/admin-route-undelivered-items/admin-route-undelivered-items.component.ts
+++ src/app/admin-route-undelivered-items/admin-route-undelivered-items.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MainService } from 'app/services/main.service';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-admin-route-undelivered-items',
@@ -8,17 +9,43 @@ import { Observable } from 'rxjs';
   styleUrls: ['./admin-route-undelivered-items.component.css']
 })
 export class AdminRouteUndeliveredItemsComponent implements OnInit {
+  displayedColumns = ['name', 'preferred_name', 'item_description']
+  undeliveredItems: any[] = [];
+  dataSource: MatTableDataSource<any>;
 
-  undeliveredItems: Observable<any>[] = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private service: MainService) { }
+  constructor(private mainService: MainService, private router: Router) { 
+    this.mainService.getAdminRouteUndeliveredItems().subscribe(data => {
+      this.undeliveredItems = data;
+      this.dataSource = new MatTableDataSource(this.undeliveredItems);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      }, error => console.log(error));
+    }
+  
 
   ngOnInit() {
-    this.service.getAdminRouteUndeliveredItems().subscribe(data => {
-      this.undeliveredItems = data, error => {
-        console.log(error);
-      }
-    });
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
+
+  // viewItems(theItems) {
+  //   localStorage.setItem('selectedItem', JSON.stringify(theItems.id));
+  //   this.router.navigate(['/serviceItem']);
+  // }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  // back() {
+  //   this.router.navigate(['/adminHome']);
+  // }
 
 }
