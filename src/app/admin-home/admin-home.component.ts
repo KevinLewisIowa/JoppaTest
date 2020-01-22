ee831de8222b5dae7dd3ae681ba07807e8dda226
Tim Wilson <wilsont3@central.edu>
2020-01-21 19:24:32 -0600

    Correctly hide/show Admin Home and End Route buttons on admin pages

--- src/app/admin-home/admin-home.component.ts
+++ src/app/admin-home/admin-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Appearance } from 'app/models/appearance';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { MainService } from 'app/services/main.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -10,12 +11,17 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 })
 export class AdminHomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private mainService: MainService, private router: Router) { }
 
   backIcon = faChevronLeft;
-
+  isAdmin: boolean;
+  routeInstanceId: number;
 
   ngOnInit() {
+    this.isAdmin = JSON.parse(window.localStorage.getItem('isAdmin'));
+    this.mainService.showAdminHome.next(this.isAdmin);
+    this.routeInstanceId = JSON.parse(window.localStorage.getItem('routeInstance'));
+    this.mainService.showEndRoute.next(this.routeInstanceId != null);
   }
 
   createNewHeater() {
--- src/app/admin-reports/admin-reports.component.ts
+++ src/app/admin-reports/admin-reports.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { MainService } from 'app/services/main.service';
 
 @Component({
   selector: 'app-admin-reports',
@@ -9,12 +10,17 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 })
 export class AdminReportsComponent implements OnInit {
 
-  constructor(private router: Router) { };
+  constructor(private mainService:MainService, private router: Router) { };
 
   backIcon = faChevronLeft;
-
+  isAdmin: boolean;
+  routeInstanceId: number;
 
   ngOnInit() {
+    this.isAdmin = JSON.parse(window.localStorage.getItem('isAdmin'));
+    this.mainService.showAdminHome.next(this.isAdmin);
+    this.routeInstanceId = JSON.parse(window.localStorage.getItem('routeInstance'));
+    this.mainService.showEndRoute.next(this.routeInstanceId != null);
   }
 
   openNewClientsReport() {
