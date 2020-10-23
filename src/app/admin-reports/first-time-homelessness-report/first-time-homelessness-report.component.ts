0e7a6a8d0d5042a058e42c650f3e6b8c80ce6e0a
Tim Wilson <wilsont3@central.edu>
2020-10-23 12:01:11 -0500

    Turn First Time Homelessness Report into data table

--- src/app/admin-reports/first-time-homelessness-report/first-time-homelessness-report.component.ts
+++ src/app/admin-reports/first-time-homelessness-report/first-time-homelessness-report.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { MainService } from 'app/services/main.service';
+import { MatPaginator, MatSort, MatTableDataSource, MatTable } from '@angular/material';
 
 @Component({
   selector: 'app-first-time-homelessness-report',
@@ -8,15 +9,35 @@ import { MainService } from 'app/services/main.service';
   styleUrls: ['./first-time-homelessness-report.component.css']
 })
 export class FirstTimeHomelessnessReportComponent implements OnInit {
+  displayedColumns = ['name', 'phone', 'birth_date', 'first_time_homeless', 'date_became_homeless', 'homeless_reason']
   clients: any[] = [];
+  dataSource: MatTableDataSource<any>;
   backIcon = faChevronLeft;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private service: MainService) { };
 
   ngOnInit() {
     this.service.getFirstTimeHomelessnessReport().subscribe(data => {
       this.clients = data;
+      this.dataSource = new MatTableDataSource(this.clients);
+      this.dataSource.sort = this.sort;
+      this.dataSource.sortingDataAccessor = (item, property) => {
+        switch (property) {
+          case 'updated_at': return new Date(item.last_interaction_date);
+          default: return item[property];
+        }
+      };
+      this.dataSource.paginator = this.paginator;
     }, error => {console.log(error)});
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
   }
 
 }
