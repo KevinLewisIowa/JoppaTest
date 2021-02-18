1c0b08b74e2cfff1bd79e1dd939d2e01457bc192
unknown <ebeth.luong@gmail.com>
2021-02-18 16:35:18 -0600

    created adminclientinactiveupdated component

---
+++ src/app/admin-client-inactive-updater/admin-client-inactive-updater.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-client-inactive-updater',
+  templateUrl: './admin-client-inactive-updater.component.html',
+  styleUrls: ['./admin-client-inactive-updater.component.css']
+})
+export class AdminClientInactiveUpdaterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
