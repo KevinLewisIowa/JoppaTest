cff81355881fb09190ef9d20e976590c40f397d2
Nic Addelia <addelia.nicolas@gmail.com>
2021-08-25 16:17:04 -0500

    added Position column to Admin Camp Listing within textbox and update button

--- src/app/admin-camp-listing/admin-camp-listing.component.ts
+++ src/app/admin-camp-listing/admin-camp-listing.component.ts
@@ -1,8 +1,9 @@
-import { ViewChild } from "@angular/core";
+import { ViewChild, Output, EventEmitter } from "@angular/core";
 import { Component, OnInit } from "@angular/core";
 import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
 import { Router } from "@angular/router";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { LocationCamp } from "app/models/location-camp";
 import { MainService } from "app/services/main.service";
 
 @Component({
@@ -11,8 +12,10 @@ import { MainService } from "app/services/main.service";
   styleUrls: ["./admin-camp-listing.component.css"],
 })
 export class AdminCampListingComponent implements OnInit {
-  displayedColumns = ["camp_name", "route_name", "is_active"];
+  displayedColumns = ["camp_name", "route_name", "position", "is_active"];
   camps: any[] = [];
+  camp: LocationCamp;
+  @Output() editedCamp = new EventEmitter<LocationCamp>();
   dataSource: MatTableDataSource<any>;
   backIcon = faChevronLeft;
 
@@ -38,6 +41,14 @@ export class AdminCampListingComponent implements OnInit {
   viewCamp(theCamp) {
     window.localStorage.setItem("routeId", JSON.stringify(theCamp.route_id));
     this.router.navigate([`/locationCamp/${theCamp.id}`]);
+  }
+
+  submitCamp(camp) {
+    console.log(camp);
+    this.mainService.updateLocationCamp(camp as LocationCamp).subscribe(data => {
+      window.localStorage.setItem('routeId', JSON.stringify(data.route_id));
+      this.editedCamp.emit(camp as LocationCamp);
+    }, error => console.log(error));
   }
 
   applyFilter(filterValue: string) {
