ca9f1937365f735c830efde6bfdacc277af05000
Tim Wilson <wilsont3@central.edu>
2021-11-16 22:22:32 -0600

    Update to Angular 9

--- src/app/models/camping-equipment.ts
+++
@@ -1,7 +0,0 @@
-export class CampingEquipment {
-  id: number = undefined;
-  name: string = undefined;
-  is_available: boolean = undefined;
-  created_at: Date = undefined;
-  updated_at: Date = undefined;
-}
