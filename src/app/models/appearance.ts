c89e158761e6299fe1a4668bd7ac4cc1bf1806b9
Tim Wilson <wilsont3@central.edu>
2021-12-11 08:42:32 -0600

    Add at_homeless_resource_center to appearance class

--- src/app/models/appearance.ts
+++ src/app/models/appearance.ts
@@ -5,4 +5,5 @@ export class Appearance {
   serviced: boolean = undefined;
   was_seen: boolean = undefined;
   still_lives_here: boolean = undefined;
+  at_homeless_resource_center: boolean = undefined;
 }
