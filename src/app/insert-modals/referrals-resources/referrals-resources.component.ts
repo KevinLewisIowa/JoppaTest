79cbc2640772ee75aeb6cf13e3a7fc053c487097
Nic Addelia <addelia.nicolas@gmail.com>
2022-01-10 13:36:30 -0600

    referrals / resources front end built out, some api functionality with errors

---
+++ src/app/insert-modals/referrals-resources/referrals-resources.component.ts
@@ -0,0 +1,63 @@
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+import { ReferralsResources } from "app/models/referrals-resources";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ClientService } from "app/services/client.service";
+
+@Component({
+  selector: "app-referrals-resources",
+  templateUrl: "./referrals-resources.component.html",
+  styleUrls: ["./referrals-resources.component.css"],
+})
+export class ReferralsResourcesComponent implements OnInit {
+  @ViewChild("referralsResourcesMdl", { static: false })
+  referralsResourcesMdl: ElementRef;
+  @Output() referralsResourcesAdded = new EventEmitter<ReferralsResources>();
+  type: string = "";
+  quantity: number;
+  notes: string = "";
+
+  constructor(private modalService: NgbModal, private service: ClientService) {}
+
+  ngOnInit() {}
+
+  showModal() {
+    this.modalService.open(this.referralsResourcesMdl, {
+      size: "lg",
+      backdrop: "static",
+    });
+  }
+
+  submitReferralsResources() {
+    const referralResource = new ReferralsResources();
+    const clientId = localStorage.getItem("selectedClient");
+    if (this.type != null && !isNaN(Number(clientId))) {
+      referralResource.type = this.type;
+      referralResource.client_id = Number(clientId);
+      referralResource.date = new Date();
+      referralResource.quantity = this.quantity;
+      referralResource.notes = this.notes;
+
+      console.log(
+        `referral/resource to insert ${JSON.stringify(referralResource)}`
+      );
+
+      this.service.insertClientReferralResource(referralResource).subscribe(
+        (data: ReferralsResources) => {
+          if (data != null && data.id != null) {
+            this.referralsResourcesAdded.emit(data);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+}
---
+++ src/app/models/referrals-resources.ts
@@ -0,0 +1,8 @@
+export class ReferralsResources {
+  id: number = undefined;
+  date: Date = undefined;
+  client_id: number = undefined;
+  type: string = undefined;
+  quantity: number = undefined;
+  notes: string = undefined;
+}
