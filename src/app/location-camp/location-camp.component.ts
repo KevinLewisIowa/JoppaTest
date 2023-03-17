11ec2b4831b0b4dca0232f1da19298de1ba45643
Tim Wilson <wilsont3@central.edu>
2023-03-17 15:15:36 -0500

    Add logging to see if we can catch when can't get clients per camp

--- src/app/location-camp/location-camp.component.ts
+++ src/app/location-camp/location-camp.component.ts
@@ -98,6 +98,7 @@ export class LocationCampComponent implements OnInit {
                 this.clientService.getClientDwellings(client.id).subscribe((data: ClientDwelling[]) => {
                   let dwellingDates = data.map(dwelling => dwelling.created_at);
                   client.dwelling = data.filter(dwelling => dwelling.created_at === dwellingDates.reduce((a, b) => a > b ? a : b))[0].dwelling;
+                  console.log(`Client: ${client.first_name} ${client.last_name}; Dwelling: ${client.dwelling}`);
 
                   if (this.heatRoute) {
                     if (client.dwelling == "Tent" || client.dwelling == "Garage" || client.dwelling == "Shack" || client.dwelling == "Camper" || client.dwelling == "Broken Down Van") {
