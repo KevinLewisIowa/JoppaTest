01e2fc19e3989f2d780e77a512a562a54669f699
Tim Wilson <wilsont3@central.edu>
2025-04-03 22:20:50 -0500

    Remove location clients from the app.module.ts file; make the footer responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,6 @@ import { AppRoutingModule } from "./app-routing.module";
 import { MainService } from "./services/main.service";
 import { ClientService } from "./services/client.service";
 import { CountryStateCityService } from "./services/countrystatecity.service";
-import { LocationClientsComponent } from "./location-clients/location-clients.component";
 import { ClientComponent } from "./client/client.component";
 import { ClientEditComponent } from "./client/client-edit/client-edit.component";
 import { RouteEditModalComponent } from "./routes/route-edit-modal/route-edit-modal.component";
@@ -108,7 +107,6 @@ const reducers = {
         FooterComponent,
         RoutesComponent,
         LocationsComponent,
-        LocationClientsComponent,
         ClientComponent,
         ClientEditComponent,
         RouteEditModalComponent,
